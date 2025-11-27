0cfd29de51ae84c7db6d36a96bb6671bcabde044
ykiko <ykikoykikoykiko@gmail.com>
2025-11-27 23:51:26 +0800

    update

---
+++ editors/vscode/src/setting.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+
+interface Setting {
+    executable: string | undefined,
+    mode: string,
+    host: string,
+    port: number,
+}
+
+export function getSetting(): Setting | undefined {
+    const setting = vscode.workspace.getConfiguration('clice')
+    const executable = setting.get<string>('executable');
+    const mode = setting.get<string>('mode');
+
+    if (mode !== "pipe" && mode !== "socket") {
+        vscode.window.showErrorMessage(`Unexpected mode: ${mode}`);
+        return undefined
+    }
+
+    const host = setting.get<string>('host')!;
+    const port = setting.get<number>('port')!;
+
+    return {
+        executable, mode, host, port,
+    }
+}
