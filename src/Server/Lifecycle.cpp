f89793c66d6d09121df64049ad48994837dd10ee
Myriad-Dreamin <camiyoru@gmail.com>
2026-01-11 03:48:09 +0800

    dev: more lifetime hooks

--- src/Server/Lifecycle.cpp
+++ src/Server/Lifecycle.cpp
@@ -114,18 +114,35 @@ async::Task<json::Value> Server::on_initialize(proto::InitializeParams params) {
 }
 
 async::Task<> Server::on_initialized(proto::InitializedParams) {
+    /// Run initialized hooks.
+    for(auto& hook: initialized_hooks) {
+        co_await hook();
+    }
+
     indexer.load_from_disk();
     co_await indexer.index_all();
+
     co_return;
 }
 
 async::Task<json::Value> Server::on_shutdown(proto::ShutdownParams params) {
+    /// Run shutdown hooks.
+    for(auto& hook: shutdown_hooks) {
+        co_await hook();
+    }
+
     co_return json::Value(nullptr);
 }
 
 async::Task<> Server::on_exit(proto::ExitParams params) {
+    /// Run exit hooks.
+    for(auto& hook: exit_hooks) {
+        co_await hook();
+    }
+
     save_cache_info();
     indexer.save_to_disk();
+
     async::stop();
     co_return;
 }
