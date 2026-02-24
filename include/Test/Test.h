3b23df2c71a13375a6bfc8c9633393e512af82fe
ykiko <ykikoykikoykiko@gmail.com>
2026-02-24 16:07:19 +0800

    clean old code

--- include/Test/Test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <print>
-#include <source_location>
-#include <string>
-#include <vector>
-
-#include "Platform.h"
-#include "eventide/zest/macro.h"
--- tests/unit/unit_tests.cc
+++ tests/unit/unit_tests.cc
@@ -1,55 +1,6 @@
-#include "Test/Test.h"
-#include "Support/FileSystem.h"
-#include "Support/GlobPattern.h"
-#include "Support/Logging.h"
+#include <iostream>
 
-#include "llvm/ADT/SmallString.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/Signals.h"
-
-using namespace clice;
-using namespace clice::testing;
-
-namespace {
-
-namespace cl = llvm::cl;
-
-cl::OptionCategory unittest_category("clice Unittest Options");
-
-cl::opt<std::string> test_dir{
-    "test-dir",
-    cl::desc("Specify the test source directory path"),
-    cl::value_desc("path"),
-    cl::Required,
-    cl::cat(unittest_category),
-};
-
-cl::opt<std::string> test_filter{
-    "test-filter",
-    cl::desc("A glob pattern to run subset of tests"),
-    cl::cat(unittest_category),
-};
-
-cl::opt<bool> enable_example{
-    "enable-example",
-    cl::init(false),
-    cl::cat(unittest_category),
-};
-
-}  // namespace
-
-int main(int argc, const char* argv[]) {
-    llvm::sys::PrintStackTraceOnErrorSignal(argv[0]);
-    llvm::cl::HideUnrelatedOptions(unittest_category);
-    llvm::cl::ParseCommandLineOptions(argc, argv, "clice test\n");
-
-    logging::stderr_logger("clice", logging::options);
-
-    if(auto result = fs::init_resource_dir(argv[0]); !result) {
-        std::println("Failed to get resource directory, because {}", result.error());
-        return 1;
-    }
-
-    using namespace clice::testing;
-    return eventide::zest::Runner::instance().run_tests(test_filter);
+int main() {
+    std::cout << "unit tests migration stub\n";
+    return 0;
 }
