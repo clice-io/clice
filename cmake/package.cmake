2a59f2390cb7b4918368e8ba2937f509f475b295
ykiko <ykikoykikoykiko@gmail.com>
2026-07-06 09:55:02 +0800

    chore: bump kotatsu pin for meta::lt map ordering

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -30,7 +30,7 @@ set(ENABLE_ROARING_MICROBENCHMARKS OFF CACHE INTERNAL "" FORCE)
 FetchContent_Declare(
     kotatsu
     GIT_REPOSITORY https://github.com/clice-io/kotatsu
-    GIT_TAG b4620e9
+    GIT_TAG 5882279
 )
 
 set(KOTA_ENABLE_ZEST ON)
